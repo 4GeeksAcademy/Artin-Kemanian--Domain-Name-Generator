663f25bf799064324d583d369d1f056d8de680d1
Artin-Kemanian <126287016+Artin-Kemanian@users.noreply.github.com>
2023-04-15 01:40:59 +0000

    segundo commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          console.log(pronoun[i]+adj[j]+noun[k]+'.com')
+        }
+      }
+    }
+  };
